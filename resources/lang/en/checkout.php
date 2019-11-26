e89b7f0203720293cac05458000cf877719c57e3
hoanghuetm <kidinu@hotmail.com>
2019-11-26 14:57:00 +0700

    Design header and footer for client pages

---
+++ resources/lang/en/checkout.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Header Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in checkout pages.
+    |
+    */
+    'view' => 'Checkout',
+];
