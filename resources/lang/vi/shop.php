50ae78832fb8fbeb9c2b73aef1fd02c4fc377e5c
hoanghuetm <kidinu@hotmail.com>
2019-11-26 14:57:00 +0700

    Design header and footer for client pages

---
+++ resources/lang/vi/shop.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Header Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in header part of client pages.
+    |
+    */
+    'title' => 'Cửa hàng Fabulous',
+    'search' => 'Tìm kiếm',
+    'slide1' => 'Hiển thị nhiều hơn, cho trải nghiệm giải trí chân thực',
+    'slide2' => 'Quyền năng để thỏa thích trải nghiệm suốt ngày dài',
+    'address' => 'Tòa nhà 9F HTP, 434 Trần Khát Chân, Hà Nội',
+    'follow' => 'Theo dõi tại',
+    'card' => 'Thẻ'
+];
